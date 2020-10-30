fd783a6d53e97edc2dc794c57eef5e613651fc63
jpassgo <jeffpascoe9@gmail.com>
2020-10-29 19:27:00 -0500

    Simple gradle plugin.

---
+++ src/main/java/HelloWorldPlugin.java
@@ -0,0 +1,10 @@
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class HelloWorldPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+        project.task("hello")
+                .doLast(task -> System.out.println("Hello World!"));
+    }
+}
